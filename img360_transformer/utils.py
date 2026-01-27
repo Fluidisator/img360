192a6cd35b29ab4578d91ba298a4ec6a12b352b1
Romain <wycvhrt6vzscfpedxr@gmail.com>
2026-01-27 14:22:18 +0100

    Change comment
    Co-authored-by: Fluidisator <70054990+Fluidisator@users.noreply.github.com>

--- img360_transformer/utils.py
+++ img360_transformer/utils.py
@@ -25,7 +25,7 @@ def rotate_360_image(img, pitch, yaw, roll):
         [[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]]
     )
 
-    R = Rz @ Ry @ Rx  # Apply Roll → Yaw → Pitch order
+    R = Rz @ Ry @ Rx  # Apply Yaw → Pitch → Roll order
 
     lon = (np.linspace(0, width - 1, width) / width) * 2 * np.pi - np.pi
     lat = (np.linspace(0, height - 1, height) / height) * np.pi - np.pi / 2
