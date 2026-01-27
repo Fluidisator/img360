d152b5fbe3db1e5186f127ecf92f8081d131cfe0
Fluidisator <70054990+Fluidisator@users.noreply.github.com>
2026-01-27 09:11:50 +0100

    Update utils.py to correct roll pitch yaw axis

--- img360_transformer/utils.py
+++ img360_transformer/utils.py
@@ -12,17 +12,17 @@ def rotate_360_image(img, pitch, yaw, roll):
     Rx = np.array(
         [
             [1, 0, 0],
-            [0, np.cos(pitch), -np.sin(pitch)],
-            [0, np.sin(pitch), np.cos(pitch)],
+            [0, np.cos(roll), -np.sin(roll)],
+            [0, np.sin(roll), np.cos(roll)],
         ]
     )
 
     Ry = np.array(
-        [[np.cos(yaw), 0, np.sin(yaw)], [0, 1, 0], [-np.sin(yaw), 0, np.cos(yaw)]]
+        [[np.cos(pitch), 0, np.sin(pitch)], [0, 1, 0], [-np.sin(pitch), 0, np.cos(pitch)]]
     )
 
     Rz = np.array(
-        [[np.cos(roll), -np.sin(roll), 0], [np.sin(roll), np.cos(roll), 0], [0, 0, 1]]
+        [[np.cos(yaw), -np.sin(yaw), 0], [np.sin(yaw), np.cos(yaw), 0], [0, 0, 1]]
     )
 
     R = Rz @ Ry @ Rx  # Apply Roll → Yaw → Pitch order
